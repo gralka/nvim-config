bca445e934d71073d1f03619f84209ee7cea21ab
J.T. Gralka <contact@jtgralka.com>
2022-12-22 09:02:11 -0500

    chore: update keymap for viewing signature help

--- lua/after/lsp.lua
+++ lua/after/lsp.lua
@@ -27,7 +27,7 @@ lsp.on_attach(function (client, buffer)
   km.set("n", "<leader>vca", function () vim.lsp.buf.code_action() end, opts)
   km.set("n", "<leader>vrr", function () vim.lsp.buf.references() end, opts)
   km.set("n", "<leader>vrn", function () vim.lsp.buf.rename() end, opts)
-  km.set("n", "<C-h>", function () vim.lsp.buf.signature_help() end, opts)
+  km.set("n", "<leader>vh", function () vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
