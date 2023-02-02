abb8899196e0d1c22e102a2d5b0c9394f598c96e
J.T. Gralka <contact@jtgralka.com>
2023-02-02 11:22:56 -0500

    chore: add <C-d> mapping for Codeium completion

--- lua/packer-setup.lua
+++ lua/packer-setup.lua
@@ -26,8 +26,9 @@ return require('packer').startup(function (use)
   use {
     'Exafunction/codeium.vim',
     config = function ()
-      -- Change '<C-g>' here to any keycode you like.
       vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.unbind('i', '<C-d>')
+      vim.keymap.set('i', '<C-d>', function () return vim.fn['codeium#Accept']() end, { expr = true })
       vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
       vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
       vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
