690d7b2dfbb3d825bb47c008e6f4b804b8de1690
J.T. Gralka <contact@jtgralka.com>
2023-02-03 09:13:33 -0500

    chore: update vim.keymap call for codeium

--- lua/packer-setup.lua
+++ lua/packer-setup.lua
@@ -27,7 +27,6 @@ return require('packer').startup(function (use)
     'Exafunction/codeium.vim',
     config = function ()
       vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.unbind('i', '<C-d>')
       vim.keymap.set('i', '<C-d>', function () return vim.fn['codeium#Accept']() end, { expr = true })
       vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
       vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
